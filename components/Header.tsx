193d8f2012563824327d1585bc5556bc411926ec
yejwoo <yejinwoo.me@gmail.com>
2023-11-27 03:03:09 +0900

    feat: modify projects content

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,9 +5,9 @@ import { Link } from "react-scroll";
 
 export default function Header() {
   return (
-    <header className="bg-white fixed w-full z-10 ">
+    <header className="bg-white fixed w-full z-10">
       <nav
-        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        className="mx-auto flex max-w-7xl items-center justify-between p-4 lg:px-8"
         aria-label="Global"
       >
         <div className="flex lg:flex-1">
